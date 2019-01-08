6b412bfd6d0efe7ecd8c0ebc4c3dc7c373e239c3
anyonecancode <pschwei1@gmail.com>
2019-01-07 21:22:06 -0500

    Sets up the header styles

--- src/components/header.js
+++ src/components/header.js
@@ -3,32 +3,11 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const Header = ({ siteTitle, siteDescription }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-          {siteDescription}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <h1>
+      <Link to="/">{siteTitle}</Link>
+    </h1>
+    <h2>{siteDescription}</h2>
   </header>
 )
 
