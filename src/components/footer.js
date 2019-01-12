fc1b129cfe62f8181effc57fd093e6eae7c59358
anyonecancode <pschwei1@gmail.com>
2019-01-12 12:06:35 -0500

    Updates footer content and style

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Footer = () => (
   <footer>
-    <p>Linked In | Github | email</p>
     <p>
-      © 2019, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+      Philip Schweiger&rsquo;s web site, built with{' '}
+      <a href="https://www.gatsbyjs.org">Gatsby</a> and hosted on{' '}
+      <a href="http://github.com/anyonecancode/anyonecancode.github.io">
+        Github
+      </a>
+      .
+    </p>
+    <p>
+      You can reach my through{' '}
+      <a href="https://www.linkedin.com/in/philipschweiger/">LinkedIn</a> or at
+      the email on my <a href="tkputk">resume</a>.
     </p>
   </footer>
 )
