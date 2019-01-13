7902782cdd6a21ccba0f459633aa1c0138b67c7c
anyonecancode <pschwei1@gmail.com>
2019-01-13 18:37:00 -0500

    Moves header data query into the header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,30 @@
 import { Link } from 'gatsby'
-import PropTypes from 'prop-types'
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 
-const Header = ({ siteTitle, siteDescription }) => (
-  <header>
-    <h1>
-      <Link to="/">{siteTitle}</Link>
-    </h1>
-    <h2>{siteDescription}</h2>
-  </header>
+const Header = () => (
+  <StaticQuery
+    query={graphql`
+      query SiteMetaDataQuery {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <header>
+          <h1>
+            <Link to="/">{data.site.siteMetadata.title}</Link>
+          </h1>
+          <h2>{data.site.siteMetadata.description}</h2>
+        </header>
+      </>
+    )}
+  />
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,37 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
-
 import Header from './header'
 import Nav from './nav'
 import Footer from './footer'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-            description
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header
-          siteTitle={data.site.siteMetadata.title}
-          siteDescription={data.site.siteMetadata.description}
-        />
-        <div id="container">
-          <main>{children}</main>
-          <Nav />
-        </div>
-        <Footer />
-      </>
-    )}
-  />
+  <>
+    <Header />
+    <div id="container">
+      <main>{children}</main>
+      <Nav />
+    </div>
+    <Footer />
+  </>
 )
 
 Layout.propTypes = {
