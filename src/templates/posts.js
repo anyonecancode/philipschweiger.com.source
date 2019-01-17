5732ef0cd0ced2bdd1ee540da08afde4e693f5fa
anyonecancode <pschwei1@gmail.com>
2019-01-16 21:10:04 -0500

    Sets up the posts template

---
+++ src/templates/posts.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+
+export default function Template({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data // data.markdownRemark holds our post data
+  const { frontmatter, html } = markdownRemark
+  return (
+    <Layout>
+      <article>
+        <header>
+          <h1>{frontmatter.title}</h1>
+          <h2>{frontmatter.date}</h2>
+        </header>
+        <section
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </article>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`
